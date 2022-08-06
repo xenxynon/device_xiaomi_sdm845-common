caded6ec2b33bc34fc5298dc63ad007ddbe49d45
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-08-06 23:51:14 +0300

    sdm845-common: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    https://github.com/PixelExp-Project/vendor_aosp/commit/c3b9f5c170c794966bb941282c3f9f18baa6279a
    
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -416,5 +416,9 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
